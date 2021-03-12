ddf07a5276ad3bc1830e24b067845f7dd2db39bb
vera <poletaeva.vera.vladimirovna@gmail.com>
2021-03-12 21:46:25 +0300

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    if (n === 0) {
+        return n;
+    }
+    if (n<0) {
+        n=n*-1;
+    }
+    let res='';
+    const str = n.toString();
+    let i=str.length;
+   while (i > 0) {
+      res=res+str[i-1];
+      i--;
+  }
+  return res;
 }
